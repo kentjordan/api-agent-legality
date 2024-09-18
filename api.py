3413c8d8a1766e2ed68d4a1f6abb214e3804e077
Kent Jordan <kentaki.dev@gmail.com>
2024-09-18 22:57:46 +0800

    fix: stream response

--- api.py
+++ api.py
@@ -1,6 +1,3 @@
-# from dotenv import load_dotenv
-# import os
-
 from collections.abc import Iterator
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
@@ -16,10 +13,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from dto.Chat import Chat
 
-# load_dotenv()
-
-# API_KEY = os.getenv("GROQ_KEY")
-
 llm = ChatOllama(model="gemma:2b")
 
 server = FastAPI()
@@ -33,8 +26,6 @@ server.add_middleware(
 )
 
 
-def full_text(iter: Iterator[BaseMessageChunk]):
-    return "".join([res.content for res in list(iter)])
 
 
 @server.get("/")
@@ -44,8 +35,18 @@ def root():
 
 chat_history = [
     SystemMessage(
-        content="You are a lawyer that knows all the knowledge of the Philippines Laws or the Repulic Acts of the Philippines."
-    )
+        content="You are an AI but be a lawyer that provides legal assitance, advice, and knows all the knowledge of the Philippines' Statues including the Acts, Commonwealth Acts, Mga Batas Pambansa, and Republic Acts"
+    ),
+    SystemMessage(
+        content="You also know the Constituion of the Philippines including 1986 Constitution, 1987 Constitution, 1973 Constitution, 1943 Constitution, 1934 Constitution, and the Malolos Constitution."
+    ),
+    SystemMessage(
+        content="Answer the problem or concern of the human, person or user as being a lawyer. Give legal assitance and advice."
+    ),
+    SystemMessage(
+        content="Be kind and objective to your answer. If you do not know the answer, just say you do not know."
+    ),
+    SystemMessage(content="Do not respond to your own chats or messages"),
 ]
 
 
@@ -54,19 +55,21 @@ def conversation():
     pass
 
 
+def llm_stream(llm_respose: Iterator[BaseMessageChunk]):
+    ai_message = ""
+    for res in llm_respose:
+        ai_message += res.content
+        yield res.content
+    chat_history.append(AIMessage(content=ai_message))
+
+
 @server.post("/chat")
 def chat(chat: Chat):
     human_message = chat.human_message
 
     chat_history.append(HumanMessage(content=human_message))
 
-    res_stream = llm.stream(chat_history)
-    res_stream_list = list(res_stream)  # Copy to list
-    res_stream = (  # Recreate the generator object
-        res.content for res in res_stream_list
-    )
-
-    chat_history.append(AIMessage(content=full_text(res_stream_list)))
+    res_stream = llm_stream(llm.stream(chat_history))
 
     return StreamingResponse(
         res_stream,
